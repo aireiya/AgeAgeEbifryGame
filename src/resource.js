965e7e6268574451cbbf5b08b5dcb5a9f5c818f7
masakazu yamaya <yamaya01masakazu04@gmail.com>
2016-07-14 10:41:01 +0900

    画像差し替え

--- src/resource.js
+++ src/resource.js
@@ -1,8 +1,8 @@
 var res = {
     background_png : "res/background.png",
-    ship_png : "res/ship.png",
+    ship_png : "res/shrimp01.png",
     particle_png : "res/particle.png",
-    asteroid_png : "res/asteroid.png",
+    asteroid_png : "res/coral_under.png",
     bgm_main : "res/bgm_main.mp3",
     se_bang : "res/se_bang.mp3"
 };
